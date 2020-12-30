68311a2a38d786847547677759f32549fe4abab9
Afshin Mokhtari <afshinator@hotmail.com>
2020-12-30 13:37:31 -0800

    boilerplate

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Home from './src/screens/Home';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Text>Show something</Text>
+      <SafeAreaView>
+        <Home />
+      </SafeAreaView>
+    </>
+  );
+};
+
+// const styles = StyleSheet.create({
+
+// });
+
+export default App;
