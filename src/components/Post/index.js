5844c66ed4cd0de830dec549024831982150930f
Afshin Mokhtari <afshinator@hotmail.com>
2020-12-30 14:42:40 -0800

    Flesh out botom area and add some dummy data

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,16 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, TouchableWithoutFeedback} from 'react-native';
+import {View, Text, TouchableWithoutFeedback, Image} from 'react-native';
 import Video from 'react-native-video';
-
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faMusic} from '@fortawesome/free-solid-svg-icons';
 import styles from './styles';
 
+const post = {
+  user: {username: 'afshin'},
+  description: 'this is oging to be the description so good',
+  song: {name: 'valhalla', 
+  imageUri: 'https://homepages.cae.wisc.edu/~ece533/images/boat.png'},
+};
 const test =
   'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4';
 
@@ -27,10 +34,20 @@ const Post = () => {
       </TouchableWithoutFeedback>
       <View style={styles.uiContainer}>
         <View style={styles.rightContainer}>
-          <Text style={{fontSize: 20, color:'white'}}>side</Text>
+          <Text style={{fontSize: 20, color: 'white'}}>side</Text>
         </View>
         <View style={styles.bottomContainer}>
-          <Text>bottom</Text>
+          <View>
+            <Text style={styles.handle}>@{post.user.username}</Text>
+            <Text style={styles.description}>{post.description}</Text>
+
+            <View style={styles.songRow}>
+              <FontAwesomeIcon icon={faMusic} size={24} color="white" />
+              <Text style={styles.songName}>{post.song.name}</Text>
+            </View>
+          </View>
+
+          <Image style={styles.songImage} source={{uri: post.song.imageUri}} />
         </View>
       </View>
     </View>
