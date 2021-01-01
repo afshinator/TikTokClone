249f80b2ca598377551e07301bc64a5719803a48
Afshin Mokhtari <afshinator@hotmail.com>
2020-12-31 18:53:03 -0800

    Add Camera component

--- src/screens/Camera/index.js
+++ src/screens/Camera/index.js
@@ -6,42 +6,43 @@ import styles from './styles';
 import awaitAsyncGenerator from '@babel/runtime/helpers/esm/awaitAsyncGenerator';
 import {useNavigation} from '@react-navigation/native';
 
+import {RNCamera} from 'react-native-camera';
 
-export default Camera = () => {
-  return <Text>Camera</Text>
-}
+// export default Camera = () => {
+//   return <Text>Camera</Text>
+// }
 
-// const Camera = () => {
-//   const [isRecording, setIsRecording] = useState(false);
-//   const camera = useRef();
+const Camera = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const camera = useRef();
 
-//   const navigation = useNavigation();
+  const navigation = useNavigation();
 
-//   const onRecord = async () => {
-//     if (isRecording) {
-//       camera.current.stopRecording();
-//     } else {
-//       const data = await camera.current.recordAsync();
-//       navigation.navigate('CreatePost', {videoUri: data.uri});
-//     }
-//   };
+  const onRecord = async () => {
+    if (isRecording) {
+      camera.current.stopRecording();
+    } else {
+      const data = await camera.current.recordAsync();
+      navigation.navigate('CreatePost', {videoUri: data.uri});
+    }
+  };
 
-//   return (
-//     <View style={styles.container}>
-//       <RNCamera
-//         ref={camera}
-//         onRecordingStart={() => setIsRecording(true)}
-//         onRecordingEnd={() => setIsRecording(false)}
-//         style={styles.preview}
-//       />
-//       <TouchableOpacity
-//         onPress={onRecord}
-//         style={
-//           isRecording ? styles.buttonStop : styles.buttonRecord
-//         }
-//       />
-//     </View>
-//   );
-// };
+  return (
+    <View style={styles.container}>
+      <RNCamera
+        ref={camera}
+        onRecordingStart={() => setIsRecording(true)}
+        onRecordingEnd={() => setIsRecording(false)}
+        style={styles.preview}
+      />
+      <TouchableOpacity
+        onPress={onRecord}
+        style={
+          isRecording ? styles.buttonStop : styles.buttonRecord
+        }
+      />
+    </View>
+  );
+};
 
-// export default Camera;
+export default Camera;
