e04a6ffce0e2bcee6273301592ffc8fb689855cd
Afshin Mokhtari <afshinator@hotmail.com>
2020-12-31 17:44:00 -0800

    add AWS authorized Cognito user to our db

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import {SafeAreaView, StatusBar} from 'react-native';
-import { withAuthenticator } from 'aws-amplify-react-native'
+import {withAuthenticator} from 'aws-amplify-react-native';
+import {Auth, API, graphqlOperation} from 'aws-amplify';
 import Navigation from './src/navigation';
 import Home from './src/screens/Home';
+import { createUser } from './graphql/mutations';
+import { getUser } from './graphql/queries';
+
+// TODO:
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
+const getRandomImage = () => {
+  return randomImages[Math.floor(Math.random() * randomImages.length)];
+};
 
 const App = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get currently authenticated user
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      console.log('>>>> user ',userInfo)
+      if (!userInfo) {  // shouldn't happen
+        return;
+      }
+
+      // check if THE user exist in database
+      const getUserResponse = await API.graphql(
+        graphqlOperation(getUser, {id: userInfo.attributes.sub}),
+      );
+
+      if (getUserResponse.data.getUser) {
+        console.log('>>>> User already exists in database');
+        return;
+      }
+
+      // if it doesn't (it's newly registered user)
+      // then, create a new user in database
+      const newUser = {
+        id: userInfo.attributes.sub,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        imageUri: getRandomImage(),
+      };
+
+      await API.graphql(graphqlOperation(createUser, {input: newUser}));
+      console.log('>>>>> new user created ', newUser);
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -20,4 +69,4 @@ const App = () => {
 
 // });
 
-export default withAuthenticator(App)
+export default withAuthenticator(App);
